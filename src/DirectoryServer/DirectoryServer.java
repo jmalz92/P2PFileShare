b616ded18ac2117ca748a835d8fe1975024877fd
jmalz92 <malzjustin@yahoo.com>
2016-03-16 01:34:48 -0500

    Listen/Accept TCP and UDP simultaneously

--- src/DirectoryServer/DirectoryServer.java
+++ src/DirectoryServer/DirectoryServer.java
@@ -7,40 +7,113 @@
 package DirectoryServer;
 
 import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.ServerSocket;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *
  * @author Justin
+ * UDP and TCP Simultaneous listening logic referenced
+ * http://www.java2s.com/Code/Java/Network-Protocol/HandlesTCPandUDPconnectionsandprovidesexceptionhandlinganderrorlogging.htm
  */
 public class DirectoryServer {
     
     
-    public static void main(String[] args) throws IOException {
-        int port;
+   public static void main(String args[]) throws IOException {
+   
+      CharsetEncoder encoder = Charset.forName("US-ASCII").newEncoder();
 
-        try {
-            port = Integer.parseInt(args[0]);
+      int port = 13; 
+      if (args.length > 0)
+        port = Integer.parseInt(args[0]);
 
-            try {
-                ServerSocket tcpSocket = new ServerSocket(port);
-                DatagramSocket udpSocket = new DatagramSocket(port);
-                
-                System.out.println("Directory Server Started on port: " + port);
+      SocketAddress localport = new InetSocketAddress(port);
 
-                while (true) {
-                    new DirectoryThread().start();
-                }
+      //tcp channel
+      ServerSocketChannel tcpserver = ServerSocketChannel.open();
+      tcpserver.socket().bind(localport);
 
-            } catch (IOException e) {
-                System.err.println("Could not listen on port: " + args[0]);
-                System.err.println(e.getMessage());
-                System.exit(-1);
+      //udp channel
+      DatagramChannel udpserver = DatagramChannel.open();
+      udpserver.socket().bind(localport);
+
+      //selector object will do the blocking
+      tcpserver.configureBlocking(false);
+      udpserver.configureBlocking(false);
+
+      Selector selector = Selector.open();
+
+      tcpserver.register(selector, SelectionKey.OP_ACCEPT);
+      udpserver.register(selector, SelectionKey.OP_READ);
+
+      ByteBuffer receiveBuffer = ByteBuffer.allocate(0);
+
+      // Now loop forever, processing client connections
+      while (true) {
+        try { 
+            
+          // Wait for a client to connect
+          selector.select();
+
+          String date = new java.util.Date().toString() + "\r\n";
+          ByteBuffer response = encoder.encode(CharBuffer.wrap(date));
+
+          // Get the SelectionKey objects for the channels that have
+          // activity on them. These are the keys returned by the
+          // register() methods above. They are returned in a
+          // java.util.Set.
+          Set keys = selector.selectedKeys();
+
+          // Iterate through the Set of keys.
+          for (Iterator i = keys.iterator(); i.hasNext();) {
+            // Get a key from the set, and remove it from the set
+            SelectionKey key = (SelectionKey) i.next();
+            i.remove();
+
+            // Get the channel associated with the key
+            Channel c = (Channel) key.channel();
+
+            // Now test the key and the channel to find out
+            // whether something happend on the TCP or UDP channel
+            if (key.isAcceptable() && c == tcpserver) {
+              // A client has attempted to connect via TCP.
+              // Accept the connection now.
+              SocketChannel client = tcpserver.accept();
+              // If we accepted the connection successfully,
+              // the send our respone back to the client.
+              if (client != null) {
+                //TODO: TCP thread logic
+                client.close(); // close connection
+              }
+            } else if (key.isReadable() && c == udpserver) {
+              // A UDP datagram is waiting. Receive it now,
+              // noting the address it was sent from.
+              SocketAddress clientAddress = udpserver.receive(receiveBuffer);
+              // If we got the datagram successfully, send
+              // the date and time in a response packet.
+              if (clientAddress != null)
+                udpserver.send(response, clientAddress);
+              //TODO: UDP thread logic
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Please supply a valid port number");
-        }
-    }
+          }
+        } catch (java.io.IOException e) {
+          
+        } 
+      }
+    } 
+  }
+
     
-}
+    
+
